13e6aabbc86df59f9910648ef9deecff5dec0a23
Siya Yan <siyayan1222@gmail.com>
2023-02-16 13:27:54 +1100

    do some initial changes.

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from "../assets/1.png";
+import "./header.css"
+
+export const Header = () => {
+  return (
+    <div>
+        <img className="logo" src={Logo} alt="" />
+        Header
+        </div>
+  )
+}
